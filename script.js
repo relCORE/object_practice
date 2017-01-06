009ff029c81e90bb18a67f8e521d3b7934da4df0
Brian Schorn <schorn.brian@gmail.com>
2017-01-05 20:49:31 -0600

    Updated script.js after debugging

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 
 
 var evilGeniuses = new TeamStats("Evil Geniuses", "North America", "10", "5", "2nd", "Cr1t", "SumaiL", "Universe", "zai", "Arteezy");
+//Method for the TeamStats object that prints out the stats in a human readable format
+TeamStats.prototype.printStats = function(){
+  console.log(this.teamName + " is a professional DotA 2 Team from " + this.teamOrigin + ".");
+  console.log("During the Internationl 2016 they achieved " + this.wins + " wins, and suffered " + this.fails + " losses.");
+  console.log("In the end they placed " + this.place + " in the tournament.");
+  console.log("The team consists of " + this.player1 + ", "+ this.player2 + ", "+ this.player3 + ", "+ this.player4 + ", and" + this.player5);
+}
 evilGeniuses.printStats();
 
 
@@ -9,16 +16,26 @@ function TeamStats(teamName, teamOrigin, wins, losses, place, player1, player2, 
   this.teamName = teamName;
   this.teamOrigin = teamOrigin;
   this.wins = wins;
-  this.losses = losses;
+  this.fails = losses;
   this.place = place;
   this.player1 = player1;
   this.player2 = player2;
   this.player3 = player3;
   this.player4 = player4;
   this.player5 = player5;
+  // this.printStats = function(){
+  //   console.log(this.teamName + " is a professional DotA 2 Team from " + this.teamOrigin + ".");
+  //   console.log("During the Internationl 2016 they achieved " + this.wins + " wins, and suffered " + this.fails + " losses.");
+  //   console.log("In the end they placed " + this.place + " in the tournament.");
+  //   console.log("The team consists of " + this.player1 + ", "+ this.player2 + ", "+ this.player3 + ", "+ this.player4 + ", and" + this.player5);
+
+
 }
 
 //Method for the TeamStats object that prints out the stats in a human readable format
-TeamStats.prototype.printStats = function(){
-  console.log(this.teamName + " is a professional DotA 2 Team from " + this.teamOrigin + ". During the Internationl 2016 they achieved " + this.wins + " wins, and suffered " + this.losses " losses. In the end they placed " + this.place + " in the tournament. The team consists of " + this.player1 + ", "+ this.player2 + ", "+ this.player3 + ", "+ this.player4 + ", and" + this.player5);
-}
+// TeamStats.prototype.printStats = function(){
+//   console.log(this.teamName + " is a professional DotA 2 Team from " + this.teamOrigin + ".");
+//   console.log("During the Internationl 2016 they achieved " + this.wins + " wins, and suffered " + this.fails + " losses.");
+//   console.log("In the end they placed " + this.place + " in the tournament.");
+//   console.log("The team consists of " + this.player1 + ", "+ this.player2 + ", "+ this.player3 + ", "+ this.player4 + ", and" + this.player5);
+// }
